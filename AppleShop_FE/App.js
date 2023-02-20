63f211c708a5a24508fc72a43a725219ea5da44b
QuanVP <93021907+QuanVPIT@users.noreply.github.com>
2023-02-20 20:39:45 +0700

    It's true

--- AppleShop_FE/App.js
+++ AppleShop_FE/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>QUan Bao</Text>
+      <Text>Hoàng Báo Chúa</Text>
       <StatusBar style="auto" />
     </View>
   );
