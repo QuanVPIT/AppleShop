de59d9786460ad013a5ce3a97b966fe3f00dd5b2
xuanhoang <102515365+hoanghihio@users.noreply.github.com>
2023-02-20 20:38:10 +0700

    test

--- AppleShop_FE/App.js
+++ AppleShop_FE/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>I'm HOÀNG BÁO</Text>
+      <Text>QUan Bao</Text>
       <StatusBar style="auto" />
     </View>
   );
