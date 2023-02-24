2b8c1e137130b690736c9838256f56faac99c86f
QuanVP <93021907+QuanVPIT@users.noreply.github.com>
2023-02-24 10:25:14 +0700

    Merge Hoang Dev
    This reverts commit f161e061e47e04cfbb1a13ef7f8a238382028059, reversing
    changes made to 3f392c6cdc080b657df5cad3fd6bdcc5d1e851f8.

--- AppleShop_BE/routes/users.js
+++ AppleShop_BE/routes/users.js
@@ -1,61 +1,9 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var UserController = require("../modules/users/UserController");
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
-
-// /* http://localhost:3000/users/api/register */
-// router.post("/api/register", async function (req, res, next) {
-//   try {
-//     const { name, email, password, age, confirm_password } = req.body;
-//     const user = await UserController.register(  name, email,password,age,confirm_password);
-//     res.status(200).json({ user });
-//   } catch (error) {
-//     res.status(414).json({ error: error.message });
-//     console.log(error);
-//     next(error);
-//   }
-// });
-
-/* http://localhost:3000/users/api/login */
-router.post("/api/login", async function (req, res, next) {
-  try {
-    const { email, password } = req.body;
-    const user = await UserController.login(email, password);
-    if (user) {
-      res.status(200).json({ user });
-    } else {
-      res.status(401).json({ error: 'Sai email hoặc mật khẩu'});
-    }
-  } catch (error) {
-    res.status(401).json({ error: error.message });
-    console.log(error);
-    next(error);
-  }
-});
-
-/*  http://localhost:3000/users/cpanel/login  */
-router.get("/cpanel/login", async function (req, res, next) {
-  res.render("login");
-});
-
-
-router.post("/cpanel/login", async function (req, res, next) {
-  try {
-    const { email, password } = req.body;
-    if ( email == 'admin@gmail.com' && password == '123' ) {
-      res.render("index", { title: "Express", name: "Hoang" });
-    } else {
-      throw new Error("Sai email hoặc mật khẩu");
-    }
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
-
 
 module.exports = router;
