9589ae62ccde4360e8ab7fdff50061c0fb77a2af
Ky Vo <vodaihieuky@gmail.com>
2023-03-06 14:35:53 +0700

    Ky Update

--- AppleShop_FE/App.js
+++ AppleShop_FE/App.js
@@ -1,11 +1,12 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import ProfileScreen from './Scr/Profile/ProfileScreen'
+import DPScreen from './Scr/DetailProduct/DPScreen'
 
 const App = () => {
   return (
     <SafeAreaView>
-      <ProfileScreen/>
+      <DPScreen/>
     </SafeAreaView>
   )
 }
