ca2172845817864871bc91708834bdf02d49faf4
Sun04082 <100474290+Sun04082@users.noreply.github.com>
2023-03-12 07:18:32 +0700

    1

--- AppleShop_FE/Src/Home/Homepage.js
+++ AppleShop_FE/Src/Home/Homepage.js
@@ -8,12 +8,25 @@ import { TabView, SceneMap } from 'react-native-tab-view';
 
 const Homepage = () => {
   const [activeTab, setActiveTab] = useState('Tab 1');
-  const [activeTab2, setActiveTab2] = useState('Tab 1.1');
+  const [activeTab1, setActiveTab1] = useState('Tab 1.1');
+
   const handleTabPress = (tabName) => {
     setActiveTab(tabName);
   };
-  const handleTabPress2 = (tabName) => {
-    setActiveTab2(tabName);
+  const handleTabPress1 = (tabName) => {
+    setActiveTab1(tabName);
+  };
+  const tab1 = (tabName) => {
+    handleTabPress(tabName);
+    handleTabPress1('Tab 1.1');
+  };
+  const tab2 = (tabName) => {
+    handleTabPress(tabName);
+    handleTabPress1('Tab 2.1');
+  };
+  const tab3 = (tabName) => {
+    handleTabPress(tabName);
+    handleTabPress1('Tab 3.1');
   };
   return (
     <View style={styles.container}>
@@ -52,7 +65,7 @@ const Homepage = () => {
         <View style={styles.tabs}>
           <TouchableOpacity
             style={[styles.tab1, activeTab === 'Tab 1' && styles.activeTab]}
-            onPress={() => handleTabPress('Tab 1')}
+            onPress={() => tab1('Tab 1')}
           >
             <Image
               style={styles.tinyLogo5}
@@ -60,7 +73,7 @@ const Homepage = () => {
           </TouchableOpacity>
           <TouchableOpacity
             style={[styles.tab1, activeTab === 'Tab 2' && styles.activeTab]}
-            onPress={() => handleTabPress('Tab 2')}
+            onPress={() => tab2('Tab 2')}
           >
             <Image
               style={styles.tinyLogo5}
@@ -69,7 +82,7 @@ const Homepage = () => {
 
           <TouchableOpacity
             style={[styles.tab1, activeTab === 'Tab 3' && styles.activeTab]}
-            onPress={() => handleTabPress('Tab 3')}
+            onPress={() => tab3('Tab 3')}
           >
             <Image
               style={styles.tinyLogo5}
@@ -102,7 +115,7 @@ const Homepage = () => {
           <View style={styles.tabs1}>
             <TouchableOpacity
               style={[styles.tab1, activeTab === 'Tab 1.1' && styles.activeTab]}
-              onPress={() => handleTabPress2('Tab 1.1')}
+              onPress={() => handleTabPress1('Tab 1.1')}
             >
               <View style={styles.viewmac}>
                 <Text styles={styles.textmac}>MacBook</Text>
@@ -111,7 +124,7 @@ const Homepage = () => {
 
             <TouchableOpacity
               style={[styles.tab1, activeTab === 'Tab 1.2' && styles.activeTab]}
-              onPress={() => handleTabPress2('Tab 1.2')}
+              onPress={() => handleTabPress1('Tab 1.2')}
             >
               <View style={styles.viewimac}>
                 <Text style={styles.textimac}>iMac</Text>
@@ -121,7 +134,7 @@ const Homepage = () => {
 
             <TouchableOpacity
               style={[styles.tab1, activeTab === 'Tab 1.3' && styles.activeTab]}
-              onPress={() => handleTabPress2('Tab 1.3')}
+              onPress={() => handleTabPress1('Tab 1.3')}
             >
               <View style={styles.viewimac}>
                 <Text style={styles.textmac}>Mac mini</Text>
@@ -130,7 +143,7 @@ const Homepage = () => {
 
             <TouchableOpacity
               style={[styles.tab1, activeTab === 'Tab 1.4' && styles.activeTab]}
-              onPress={() => handleTabPress2('Tab 1.4')}
+              onPress={() => handleTabPress1('Tab 1.4')}
             >
               <View style={styles.viewmacdisplays}>
                 <Text style={styles.textmacdisplays}>Mac Displays</Text>
@@ -143,7 +156,45 @@ const Homepage = () => {
       {/* //tab2 */}
       {activeTab === 'Tab 2' && (
         <View style={styles.tabText1}>
+          <View style={styles.tabs1}>
+            <TouchableOpacity
+              style={[styles.tab1, activeTab === 'Tab 2.1' && styles.activeTab]}
+              onPress={() => handleTabPress1('Tab 2.1')}
+            >
+              <View style={styles.viewmac}>
+                <Text styles={styles.textmac}>Semua</Text>
+              </View>
+            </TouchableOpacity>
 
+            <TouchableOpacity
+              style={[styles.tab1, activeTab === 'Tab 2.2' && styles.activeTab]}
+              onPress={() => handleTabPress1('Tab 2.2')}
+            >
+              <View style={styles.viewimac}>
+                <Text style={styles.textimac}>iPhone 11</Text>
+              </View>
+
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={[styles.tab1, activeTab === 'Tab 2.3' && styles.activeTab]}
+              onPress={() => handleTabPress1('Tab 2.3')}
+            >
+              <View style={styles.viewimac}>
+                <Text style={styles.textmac}>iPhone 12</Text>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={[styles.tab1, activeTab === 'Tab 2.4' && styles.activeTab]}
+              onPress={() => handleTabPress1('Tab 2.4')}
+            >
+              <View style={styles.viewmacdisplays}>
+                <Text style={styles.textmacdisplays}>iPhone 13</Text>
+              </View>
+            </TouchableOpacity>
+
+          </View>
         </View>
       )}
       {/* //tab3 */}
@@ -166,8 +217,8 @@ const Homepage = () => {
 
         </View>
       )}
-   {/* tab1 nho chuyen api */}
-      {activeTab2 === 'Tab 1.1' && (
+      {/* tab1 nho chuyen api */}
+      {activeTab1 === 'Tab 1.1' && (
         <View style={{ flex: 1 }}>
 
           <Text>
@@ -175,7 +226,7 @@ const Homepage = () => {
           </Text>
         </View>
       )}
-      {activeTab2 === 'Tab 1.2' && (
+      {activeTab1 === 'Tab 1.2' && (
         <View style={{ flex: 1 }}>
 
           <Text>
@@ -183,7 +234,7 @@ const Homepage = () => {
           </Text>
         </View>
       )}
-      {activeTab2 === 'Tab 1.3' && (
+      {activeTab1 === 'Tab 1.3' && (
         <View style={{ flex: 1 }}>
 
           <Text>
@@ -191,11 +242,81 @@ const Homepage = () => {
           </Text>
         </View>
       )}
-      {activeTab2 === 'Tab 1.4' && (
+      {activeTab1 === 'Tab 1.4' && (
         <View style={{ flex: 1 }}>
           <View style={{}}>
             <Text>
               macdisplays
+            </Text>
+          </View>
+        </View>
+      )}
+     
+     {/* Tab 2.x */}
+     {activeTab1 === 'Tab 2.1' && (
+        <View style={{ flex: 1 }}>
+
+          <Text>
+            Iphone 11
+          </Text>
+        </View>
+      )}
+      {activeTab1 === 'Tab 2.2' && (
+        <View style={{ flex: 1 }}>
+
+          <Text>
+          Iphone 12
+          </Text>
+        </View>
+      )}
+      {activeTab1 === 'Tab 2.3' && (
+        <View style={{ flex: 1 }}>
+
+          <Text>
+          Iphone 13
+          </Text>
+        </View>
+      )}
+      {activeTab1 === 'Tab 2.4' && (
+        <View style={{ flex: 1 }}>
+          <View style={{}}>
+            <Text>
+            Iphone 14
+            </Text>
+          </View>
+        </View>
+      )}
+
+       {/* Tab 3.x */}
+     {activeTab1 === 'Tab 3.1' && (
+        <View style={{ flex: 1 }}>
+
+          <Text>
+            I
+          </Text>
+        </View>
+      )}
+      {activeTab1 === 'Tab 3.2' && (
+        <View style={{ flex: 1 }}>
+
+          <Text>
+          Iphone 12
+          </Text>
+        </View>
+      )}
+      {activeTab1 === 'Tab 3.3' && (
+        <View style={{ flex: 1 }}>
+
+          <Text>
+          Iphone 13
+          </Text>
+        </View>
+      )}
+      {activeTab1 === 'Tab 3.4' && (
+        <View style={{ flex: 1 }}>
+          <View style={{}}>
+            <Text>
+            Iphone 14
             </Text>
           </View>
         </View>
